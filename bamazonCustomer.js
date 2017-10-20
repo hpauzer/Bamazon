1bc6fd9ff4fd46ba4333bd37634367a4c020a9b5
hpauzer <heatherpauzer@Heathers-MacBook-Pro-2.local>
2017-10-20 16:35:07 -0500

    update js file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,6 +5,7 @@ var productArray = [];
 var product;
 var price;
 var totalPrice;
+var qtyPurchased;
 var revStock;
 var idSelectArray;
 var idSelectTable;
@@ -35,3 +36,35 @@ var stillShopping = [{
     message: "Are you ready to checkout?",
     choices: ["Yes", "No"],
 }];
+
+connection.connect(function(error) {
+    if (error) throw error;
+    selectProducts();
+})
+
+function selectProducts() {
+    connection.query("SELECT * FROM products", function(error, results) {
+        if (error) throw error;
+        console.log("Bamazon Best Sellers: ");
+
+        for (var i = 0; i < results.length; i++) {
+            productArray.push({
+                ID: results[i].item_id,
+                Product: results[i].product_name,
+                Department: results[i].department_name,
+                Price: results[i].price,
+                Stock: results[i].stock_quantity
+            });
+        }
+    })
+}
+console.table(productArray);
+console.log("------------------------");
+
+inquirer.prompt(customerPrompt).then(function(answers) {
+    idSelectArray = parseInt(answers.idPurchase - 1);
+    idSelectedTable = parseInt(answers.idPurchase);
+    qtyPurchased = parseInt(answers.unitsPurchase);
+
+
+})
